533e60622fc7d8a04ef70829c6c9c0ff857280b1
Riley Dunn <56401648+git-it-dunn@users.noreply.github.com>
2021-12-08 06:47:04 -0500

    Delete src directory

--- src/main/java/LinearHashtable.java
+++
@@ -1,9 +0,0 @@
-public class LinearHashtable<T> extends Hashtable<T> {
-    public LinearHashtable(int capacity) {
-        super(capacity);
-    }
-
-    public int rehash(int previousHash) {
-        return (previousHash + 1) % capacity;
-    }
-}
--- src/main/java/QuadraticHashtable.java
+++
@@ -1,9 +0,0 @@
-public class QuadraticHashtable<T> extends Hashtable<T> {
-    public QuadraticHashtable(int capacity) {
-        super(capacity);
-    }
-
-    public int rehash(int previousHash) {
-        return (previousHash * previousHash + 1) % capacity;
-    }
-}
